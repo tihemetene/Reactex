e126158e1d828e974d02ab448c89b12ab305d7c7
Thiago <thiago.barboza.p@gmail.com>
2021-02-12 21:30:46 -0300

    Text, style and some fixes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,11 +6,15 @@ const Caixa = ({ info, cardTitle }) => (
   <Card className="m-4">
     <Card.Header><b>{cardTitle}</b></Card.Header>
     <Card.Body className="p-2">
-      <Button variant="dark" className="p-2 casa">
+      <Button variant="dark" className="p-2 preto">
         { info }
         {' '}
       </Button>
-      <Image className="rounded mx-auto d-block " src="https://media0.giphy.com/media/LmNwrBhejkK9EFP504/200.gif" roundedCircle />
+      <div xs={6} md={4} className="text-center texto">
+        There are no items yet.
+        {' '}
+        <Image className="rounded mx-auto d-block " src="https://media0.giphy.com/media/LmNwrBhejkK9EFP504/200.gif" roundedCircle />
+      </div>
     </Card.Body>
   </Card>
 );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = ({ title, home }) => (
-  <Navbar className="casa" variant="dark">
+  <Navbar className="preto" variant="dark">
     <Navbar.Brand className="titulo" href="#home">{title}</Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link className="casa" href="#home">{home}</Nav.Link>
